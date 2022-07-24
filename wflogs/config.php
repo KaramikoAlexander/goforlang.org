e71c37934899b9f9303511b870f317d53554ef0c
Karamiko Alexander <karamiko.alexander@gmail.com>
2022-07-25 06:13:47 +0700

    Add files via upload

---
+++ wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"X8[yw;>]yQYn|+PW<AGtx}vq/o`.ugq!Rp7X>h 6 Nte)Cs$sAPYb!|X(4Z:^wnK";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3805;}
